9ab0d285abbd24211be3a51d25ea371b99321e2e
96weibin <1643960119@qq.com>
2018-10-23 12:44:20 +0800

    图片文件压缩

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HarbinTeamProject
-团队开发   经过四天时间与老黑的并肩开发
+
+
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+// var less = require('gulp-less');
+// var minifyCss = require('gulp-minify-css');
+// var uglify = require('gulp-uglify');
+// var babel = require('gulp-babel');
+var imagemin = require('gulp-imagemin');
+// gulp.task('less', function() {
+//   gulp.src('./dev/css/*.less')
+//   .pipe(less())
+//   .pipe(minifyCss())	
+//   .pipe(gulp.dest('./css'))
+// });
+// gulp.task('css', function() {
+//   gulp.src('./dev/css/*.css')
+//   .pipe(minifyCss())	
+//   .pipe(gulp.dest('./css'))
+// });
+// gulp.task('js',function(){
+//   gulp.src('./dev/js/*.*')
+//   .pipe(babel({
+//     presets: ['@babel/env']
+//   }))
+//   .pipe(uglify()) //先不压缩   不利于调试
+//   .pipe(gulp.dest('./js'))
+// });
+gulp.task('img',function(){
+  gulp.src('./dev/img/*.*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./img'))
+});
+// gulp.task('default',function(){
+//   gulp.watch('./dev/css/**/*.less',['less']);
+//   gulp.watch('./dev/css/**/*.css',['css']);
+//   gulp.watch('./dev/js/**/*.js',['js']);
+//   gulp.watch('./dev/img/**/*.*',['img']);
+// })
